c7630b9030946c80e554b727a13ff1cc3d828099
Gian <giansingh4710@gmail.com>
2022-11-08 17:10:33 -0500

    made site better lol

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { links } from "./allLinks";
 
 function App() {
   const [entry, setEntry] = React.useState("");
-  const [searchResult, setSearch] = React.useState("Enter Something");
+  const [searchResult, setSearch] = React.useState("");
 
   const getArticles = (search) => {
     const validLinks = links.filter((word) =>
@@ -11,44 +11,44 @@ function App() {
     );
     if (validLinks.length > 0) {
       return (
-        <ol>
-          {validLinks.map((word) => {
-            return (
-              <li key={word.link}>
-                <a href={word.link} target="_blank" rel="noopener noreferrer">
-                  {word.title}
-                </a>
-              </li>
-            );
-          })}
-        </ol>
+        <div>
+          {validLinks.length} results
+          <ol>
+            {validLinks.map((word) => {
+              return (
+                <li key={word.link}>
+                  <a href={word.link} target="_blank" rel="noopener noreferrer">
+                    {word.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
       );
     }
-    return "No results for: " + search;
+    return <div>
+      No results for: {search}
+    </div>
   };
+
   return (
     <div className="App">
       <div>
         <h1>Enter a question or topic:</h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            autoFocus="autofocus"
-            placeholder="Vaheguru"
-            value={entry}
-            onChange={(e) => {
-              setEntry(e.target.value);
-            }}
-          />
-          <button
-            type="submit"
-            onClick={() => {
-              const articles = getArticles(entry);
+        <input
+          autoFocus="autofocus"
+          placeholder="Vaheguru"
+          value={entry}
+          onChange={(e) => {
+            const newEntry = e.target.value
+            if (newEntry.length > entry.length) {
+              const articles = getArticles(newEntry);
               setSearch(articles);
-            }}
-          >
-            Submit
-          </button>
-        </form>
+            }
+            setEntry(newEntry);
+          }}
+        />
         {searchResult}
       </div>
     </div>
